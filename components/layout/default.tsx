f203bbb62331b6eb3a006788e4b02ae52334d791
tomoki matsubuchi <matsubuchitmk@gmail.com>
2023-09-02 17:31:12 +0900

    headerの整備とsassへの対応化

--- components/layout/default.tsx
+++ components/layout/default.tsx
@@ -1,16 +1,41 @@
 import { ReactElement } from 'react'
-import styles from '@/styles/components/layout/default.module.css'
+import styles from '@/styles/components/layout/default.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type LayoutProps = Required<{
 	readonly children: ReactElement
 }>
 
 const Layout = ({ children }: LayoutProps) => {
+	// Explain: Headerにリンクを追加する際にはこの連想配列に追加する。
+	const headerLinks = [
+		{
+			pageName: 'TOP',
+			href: '/',
+		},
+		{
+			pageName: 'プロフィール作成',
+			href: '/generate_profile/explain',
+		},
+	]
+
+	// Explain: HeaderのLinkをまとめて表示できるようにする。
+	const headerLinkList = headerLinks.map((link, index) => {
+		return (
+			<li key={index} className={styles.header_content}>
+				<Link href={link.href} className={styles.header_link}>
+					{link.pageName}
+				</Link>
+			</li>
+		)
+	})
+
 	return (
 		<div>
-			<header>
-				<Image src="/images/nature.png" width={576} height={320} alt="" />
+			<header className={styles.header_container}>
+				<h2>ProfileGenerater</h2>
+				<ul className={styles.header_list}>{headerLinkList}</ul>
 			</header>
 			<main className={styles.container}>{children}</main>
 		</div>
